5c94430f8c76791272dd7c1a995b32eaacc4f465
cbs <166548181+cbsdevl@users.noreply.github.com>
2025-10-14 13:23:28 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://rfo-fyrk.onrender.com',
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
     },
